36f8a6af3d1f7350e83549d6a25510490f198df9
Shayan Faghihi <s.faghihi@bettechnology.com>
2023-05-13 00:00:51 +0330

    Add Temp Unit Toggle Button

--- src/components/ShowResults.jsx
+++ src/components/ShowResults.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import FormControlLabel from '@mui/material/FormControlLabel';
+import { Switch } from "@mui/material";
 
-const ShowResults = ({results,error} ) => {
+const ShowResults = ({ results, error }) => {
+  const [isFarenheit, setIsFarenheit] = useState(false);
 
+  const changeTempUnit = () => {
+    setIsFarenheit(!isFarenheit);
+  };
   if (results) {
     const location = results.location.name;
     const tempC = results.current.temp_c;
@@ -11,29 +17,53 @@ const ShowResults = ({results,error} ) => {
     const humidity = results.current.humidity;
     const windSpeed = results.current.wind_kph;
 
-
     return (
       <div className="results-box">
-        <h2 className="results-box__title">Weather in {location}</h2>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <h2 className="results-box__title">Weather in {location}</h2>
+          <FormControlLabel
+          control ={
+            <Switch
+              checked={isFarenheit}
+              onChange={changeTempUnit}
+              inputProps={{ "aria-label": "controlled" }}
+              color="warning"
+              className="toggleSwitch"
+            />
+          }
+          label="℉"
+          />
+        </div>
         <div className="results-box__data">
           <div className="results-box__data--temp">
-            <span>{!isFarenheit ? tempC : tempF } </span>
-            <span>{!isFarenheit ? '℃' : '℉'}</span>
+            <span>{!isFarenheit ? tempC : tempF} </span>
+            <span>{!isFarenheit ? "℃" : "℉"}</span>
           </div>
           <div className="results-box__data--condition">
             <img
-              src={'http:' + conditionIconURL}
+              src={"http:" + conditionIconURL}
               alt="Current Location Condition Sign"
             />
             <span>{condition}</span>
           </div>
-          <div className="results-box__data--humidity">Humidity: {humidity}%</div>
-          <div className="results-box__data--wind">Wind Speed: {windSpeed} km/hr</div>
+          <div className="results-box__data--humidity">
+            Humidity: {humidity}%
+          </div>
+          <div className="results-box__data--wind">
+            Wind Speed: {windSpeed} km/hr
+          </div>
         </div>
       </div>
     );
-  } if (error) {
-    return <p className="error">{error}</p>
+  }
+  if (error) {
+    return <p className="error">{error}</p>;
   }
 };
 
